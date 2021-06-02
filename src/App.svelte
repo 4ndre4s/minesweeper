d573715772a34f3b2b981e93259e5599fb0a3d81
Andreas Partsch <public@partsch.xyz>
2021-06-02 20:03:40 +0200

    Create board of mines

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Tile from './components/Tile.svelte'
+
+  const numberOfTiles = 9
+  const TILES = Array.from(new Array(numberOfTiles), (_, i) => i + 1)
+</script>
+
+{#each TILES as tile, columnIndex}
+  <div class="row">
+    {#each TILES as rowIndex}
+      <Tile columnIndex={columnIndex} rowIndex={rowIndex}/>
+    {/each}
+  </div>
+{/each}
+
+<style>
+    .row {
+        display: flex;
+    }
+</style>
---
+++ src/components/Tile.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let rowIndex;
+  export let columnIndex;
+
+  let isCovered = true;
+
+
+  function onClick() {
+    isCovered = false;
+  }
+</script>
+
+<div class="tile" class:covered={isCovered} on:click={onClick}></div>
+
+<style>
+  .tile {
+      width: 20px;
+      height: 20px;
+      background: #cccccc;
+      border: 1px solid #6f6f6f;
+  }
+  .tile.covered {
+      width: 16px;
+      height: 16px;
+      background: #c6c6c6;
+      cursor: pointer;
+      border-top: 3px outset white;
+      border-left: 3px outset white;
+      border-bottom: 3px outset #808080;
+      border-right: 3px outset #808080;
+  }
+</style>
