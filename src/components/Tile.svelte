487b437dedfcb668fc92b08f7cd8526578b2ca35
Andreas Partsch <public@partsch.xyz>
2021-06-02 20:16:43 +0200

    Tiles can be marked as bomb

--- src/components/Tile.svelte
+++ src/components/Tile.svelte
@@ -4,13 +4,22 @@
 
   let isCovered = true;
 
-
-  function onClick() {
+  function reveal() {
+    if (isMarkedAsBomb) {
+      return;
+    }
     isCovered = false;
+  }
+
+  let isMarkedAsBomb = false;
+
+  function toggleBombMarker(event) {
+    event.preventDefault();
+    isMarkedAsBomb = !isMarkedAsBomb;
   }
 </script>
 
-<div class="tile" class:covered={isCovered} on:click={onClick}></div>
+<div class="tile" class:covered={isCovered} class:bomb={isMarkedAsBomb} on:click={reveal} on:contextmenu={toggleBombMarker}></div>
 
 <style>
   .tile {
@@ -28,5 +37,15 @@
       border-left: 3px outset white;
       border-bottom: 3px outset #808080;
       border-right: 3px outset #808080;
+  }
+  .tile.covered.bomb::before {
+      content: '';
+      display: inline-block;
+      width: .75rem;
+      height: .75rem;
+      margin-left: 1px;
+      margin-bottom: 1px;
+      border-radius: 15px;
+      background: red;
   }
 </style>
