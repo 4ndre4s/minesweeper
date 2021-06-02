108a117d67be58406a1d9386517f8a9353576db7
Andreas Partsch <public@partsch.xyz>
2021-06-02 20:06:18 +0200

    Add tile container

--- src/App.svelte
+++ src/App.svelte
@@ -5,15 +5,26 @@
   const TILES = Array.from(new Array(numberOfTiles), (_, i) => i + 1)
 </script>
 
-{#each TILES as tile, columnIndex}
-  <div class="row">
-    {#each TILES as rowIndex}
-      <Tile columnIndex={columnIndex} rowIndex={rowIndex}/>
-    {/each}
-  </div>
-{/each}
+<div class="tile-container">
+  {#each TILES as tile, columnIndex}
+    <div class="row">
+      {#each TILES as rowIndex}
+        <Tile columnIndex={columnIndex} rowIndex={rowIndex}/>
+      {/each}
+    </div>
+  {/each}
+</div>
 
 <style>
+    .tile-container {
+        display: inline-block;
+        padding: 0.5rem;
+        background: #c6c6c6;
+        border-top: 3px outset white;
+        border-left: 3px outset white;
+        border-bottom: 3px outset #808080;
+        border-right: 3px outset #808080;
+    }
     .row {
         display: flex;
     }
